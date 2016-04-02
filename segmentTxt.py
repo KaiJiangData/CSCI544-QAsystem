a8e64a7ae27ee714ee6c6883c5b1b73dc94ed865
Zhen <zli718@usc.edu>
2016-04-01 19:33:40 -0700

    Added files via upload

---
+++ segmentTxt.py
@@ -0,0 +1,35 @@
+import sys
+import re
+import os
+import codecs
+import subprocess
+
+# Put this py file in 'stanford-segmenter-2015-12-09' folder that can be downloaded from 
+# http://nlp.stanford.edu/software/segmenter.shtml
+# Usage: python segmentTxt.py input_Text_File_Name output_Text_File_Name
+
+segment_url = "./segment/segment.sh ctb "
+intermedia_buffer = "intermedia_buffer.txt"
+otherArg = " UTF-8 0"
+
+file_need_segment = open(sys.argv[1], "r")
+content = file_need_segment.read().decode('utf-8')
+file_need_segment.close()
+
+split_content = re.split(u'[\u3002\uff1b\uff01\uff1f\n]',content)
+intermedia_buffer_file = codecs.open(intermedia_buffer, 'w',"utf-8")
+for line in split_content:
+	if line==u"":
+		continue
+	intermedia_buffer_file.write(line)
+	intermedia_buffer_file.write(u'\u3002')
+	intermedia_buffer_file.write('\n')
+intermedia_buffer_file.close()
+
+segmented_file = open(sys.argv[2], 'w')
+command = segment_url + intermedia_buffer + otherArg
+subprocess.Popen(command, stdout=segmented_file, shell=True)
+segmented_file.close()
+os.remove("intermedia_buffer")
+
+
