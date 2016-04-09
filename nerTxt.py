a161de4e5a8696a26d933c7dfa43829418465978
Zhen <zli718@usc.edu>
2016-04-08 20:07:43 -0700

    Added files via upload

---
+++ nerTxt.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import subprocess
+
+#replace the original ner.sh in stanford-ner-2015-12-09 with the ner.sh I give
+#Usage: python nerTxt.py input.txt output.txt
+command=["stanford-ner-2015-12-09/ner.sh", sys.argv[1]]
+ner_file = open(sys.argv[2], 'w')
+p = subprocess.Popen(command, stdout=ner_file, shell=False)
+p.wait()
+ner_file.close()
+
+
+
