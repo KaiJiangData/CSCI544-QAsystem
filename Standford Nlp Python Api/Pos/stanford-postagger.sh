cc61cef5b87bcead50b477fdd65efe14e98cdbac
Zhen Li <zli718@usc.edu>
2016-04-29 16:27:00 -0700

    a

--- Standford Nlp Python Api/Pos/stanford-postagger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# usage: ./stanford-postagger.sh model textFile
-#  e.g., ./stanford-postagger.sh models/english-left3words-distsim.tagger sample-input.txt
-
-java -mx300m -cp 'stanford-postagger-full-2015-12-09/stanford-postagger.jar:stanford-postagger-full-2015-12-09/lib/*' edu.stanford.nlp.tagger.maxent.MaxentTagger -model $1 -textFile $2
